2b77e04eb04cebc662975f9834c4f69113997c41
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-18 13:35:03 +0200

    solution to q1

---
+++ q1.js
@@ -0,0 +1,8 @@
+"use strict"
+let number1 = Number(prompt('Enter number'));
+let number2 = Number(prompt('Enter number'));
+if(number1 > number2){
+    alert('the bigest number is: ' + number1)
+} else{
+    alert('the bigest number is: ' + number2);
+}
