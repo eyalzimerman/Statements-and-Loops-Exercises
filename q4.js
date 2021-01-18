9fc8c8c240ceaa9b69a6057ef8a6d3bdf8d8d161
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-18 14:41:00 +0200

    solution to q4

---
+++ q4.js
@@ -0,0 +1,21 @@
+"use strict";
+let number1 = 5;
+let number2 = 7;
+let number3 = 2;
+let number4 = 9;
+let number5 = 4;
+if(number1 > number2 && number1 > number3 && number1 > number4 && number1 > number5){
+    alert('the largest number is: ' + number1);
+}
+else if (number2 > number1 && number2 > number3 && number2 > number4 && number2 > number5){
+    alert('the largest number is: ' + number2);
+}
+else if(number3 > number1 && number3 > number2 && number3 > number4 && number3 > number5){
+    alert('the largest number is: ' + number3);
+}
+else if(number4 > number1 && number4 > number2 && number4 > number3 && number4 > number5){
+    alert('the largest number is: ' + number4);
+}
+else if(number5 > number1 && number5 > number2 && number5 > number3 && number5 > number4){
+    alert('the largest number is: ' + number5);
+}
